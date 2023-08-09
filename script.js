c6057f907c7567735a10e0f96c9c725f5f3d728f
Trilakshmi <140579513+Trilakshmi@users.noreply.github.com>
2023-08-09 13:26:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const inputSearch=document.getElementById("input-search");
+const listContainer=document.getElementById("listContainer");
+function addTask(){
+    if(inputSearch.value===''){
+        alert("You must write anything!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputSearch.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span")
+        span.innerHTML="\u00d7"
+        li.appendChild(span)
+    }
+    inputSearch.value=''
+    saveData();
+}
+
+
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+
+}
+
+function showList(){
+    listContainer.innerHTML=localStorage.getItem("data")
+}
+
+showList();
